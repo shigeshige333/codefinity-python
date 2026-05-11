9b62ef639fafe36cfc35d56a8faebbbbd177d229
shigeshige333 <>
2026-05-11 15:34:56 +0000

    Tuple Addition

---
+++ tuple_addition/main.py
@@ -0,0 +1,8 @@
+animal_movies = ('The Lion King', 'Jurassic Park', 'Finding Nemo')
+
+# Write your code here
+new_movies = ("Dumbo", "Zootopia")
+
+animal_movies += new_movies
+
+print("Updated animal movies:", animal_movies)
