3ad04c56eb4751083bce54d485629ba97c3c949a
shigeshige333 <>
2026-05-15 16:10:19 +0000

    Loop Control

--- loop_control/main.py
+++ loop_control/main.py
@@ -1,18 +1,16 @@
-# List of countries you are considering for travel
+# List of country names
 countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
 
-# List of countries that require a visa
-visa_required = ['China', 'India', 'Saudi Arabia', 'Brazil', 'United Arab Emirates', 'Egypt']
+# List to hold selected countries
+selected = []
 
-# List of visa-free travel destinations
-travel_list = []
+i = 0
 
-for country in countries:
-    if country not in visa_required:
-        travel_list.append(country)
-    if len(travel_list) == 10:
-        break
-
+while i < len(countries):
+    if countries[i][0] == 'S':
+        if len(selected) < 3:
+            selected.append(countries[i])
+    i += 1
 
 # Testing
-print('Visa-free travel destinations:', travel_list)
+print('First three countries starting with "S":', selected)
