e04f735a370396a51f5f5645c7f8e8c63a5b6361
shigeshige333 <>
2026-05-17 11:42:58 +0000

    List Comprehensions

--- list_comprehensions/main.py
+++ list_comprehensions/main.py
@@ -6,11 +6,11 @@ travel_wishlist = [['Paris', 'France', 2000],['Tokyo', 'Japan', 3000],['New York
                    ['Los Angeles', 'USA', 2700],['Cape Town', 'South Africa', 2300],['Venice', 'Italy', 2000],
                    ['Istanbul', 'Turkey', 1750],['Toronto', 'Canada', 2250],['Rio de Janeiro', 'Brazil', 1950],
                    ['Athens', 'Greece', 1850]]
+                   
+# Create a new list of affordable cities
+affordable_cities = []
 
-# Extract trip costs using list comprehension
-trip_costs = []
-
-trip_costs = [trip[2] for trip in travel_wishlist]
+affordable_cities = [city[0] for city in travel_wishlist if city[2] < 2500 ]
 
 # Testing
-print('Trip Costs:', trip_costs)
+print('Affordable Cities:', affordable_cities)
